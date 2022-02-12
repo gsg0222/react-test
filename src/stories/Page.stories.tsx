b5167777fc7ab44268e4ebe0f65185be412f8c5b
gsg0222 <gsg0222.1977@gmail.com>
2022-02-12 17:05:27 +0900

    Lintのエラーを無効化

--- src/stories/Page.stories.tsx
+++ src/stories/Page.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Page } from './Page';
@@ -9,6 +10,7 @@ export default {
   component: Page,
 } as ComponentMeta<typeof Page>;
 
+// eslint-disable-next-line react/jsx-props-no-spreading
 const Template: ComponentStory<typeof Page> = (args) => <Page {...args} />;
 
 export const LoggedIn = Template.bind({});
